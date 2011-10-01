ff815b6dcd2c67cf9c75203a74dc9976383f6aef
Brion Emde <brione2001@gmail.com>
2011-10-01 16:40:19 -0700

    Haven't tested the editor stuff at all. For some reason the Honecomb emulator comes up as not taking the xlarge template, or something :-)

--- src/com/eyebrowssoftware/bptrackerfree/BPListFragment.java
+++ src/com/eyebrowssoftware/bptrackerfree/BPListFragment.java
@@ -59,8 +59,6 @@ public class BPListFragment extends ListFragment implements OnClickListener {
 	public static final int MENU_ITEM_SEND = Menu.FIRST + 2;
 	public static final int MENU_DATA_MANAGER = Menu.FIRST + 3;
 	
-	private static final int DELETE_DIALOG_ID = 0;
-
 	private static final String[] VALS = { 
 		BPRecord.CREATED_DATE,
 		BPRecord.CREATED_DATE, 
@@ -87,16 +85,12 @@ public class BPListFragment extends ListFragment implements OnClickListener {
 	
 	private static final int BPRECORDS_TOKEN = 0;
 	
-	private Activity mActivity = null; // The activity this Fragment is associated with
-	
 	private Intent mIntent = null; // The intent that started the activity that this fragment is associated with
 	
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		// TODO: is this needed here? default returns a list view
-		// super.onCreateView(inflater, container, savedInstanceState);
 		
 		// Inflate the layout for this fragment
 		View layout = inflater.inflate(R.layout.bp_list_fragment, container, false);
@@ -129,8 +123,6 @@ public class BPListFragment extends ListFragment implements OnClickListener {
 	@Override
 	public void onResume() {
 		super.onResume();
-		
-		mMAQH.startQuery(BPRECORDS_TOKEN, this, mActivity.getIntent().getData(), PROJECTION, null, null, BPRecord.CREATED_DATE + " DESC");
 	}
 	
 	@Override
@@ -142,16 +134,16 @@ public class BPListFragment extends ListFragment implements OnClickListener {
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 
-		mActivity = this.getActivity();
-		mIntent = mActivity.getIntent();
+		mIntent = getActivity().getIntent();
 
 		if (mIntent.getData() == null) {
 			mIntent.setData(BPRecords.CONTENT_URI);
 		}
-		SimpleCursorAdapter adapter = new SimpleCursorAdapter(mActivity, R.layout.bp_record_list_item, null, VALS, IDS);
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(getActivity(), R.layout.bp_record_list_item, null, VALS, IDS);
 		adapter.setViewBinder(new MyViewBinder());
 
-		mMAQH = new MyAsyncQueryHandler(mActivity.getContentResolver(), adapter);
+		mMAQH = new MyAsyncQueryHandler(getActivity().getContentResolver(), adapter);
+		mMAQH.startQuery(BPRECORDS_TOKEN, this, getActivity().getIntent().getData(), PROJECTION, null, null, BPRecord.CREATED_DATE + " DESC");
 		
 		if(savedInstanceState != null) {
 			mContextMenuRecordId = savedInstanceState.getLong(CONTEXT_URI);
@@ -206,7 +198,7 @@ public class BPListFragment extends ListFragment implements OnClickListener {
 		@Override
 		protected void onQueryComplete(int token, Object cookie, Cursor cursor) {
 			if (cursor != null) {
-				mActivity.startManagingCursor(cursor);
+				getActivity().startManagingCursor(cursor);
 				SimpleCursorAdapter adapter = mAdapter.get();
 				if(adapter != null) {
 					adapter.changeCursor(cursor);
@@ -281,7 +273,7 @@ public class BPListFragment extends ListFragment implements OnClickListener {
 				startActivity(new Intent(Intent.ACTION_EDIT, contextMenuUri));
 				return true;
 			case MENU_ITEM_SEND:
-				startActivity(new Intent(Intent.ACTION_SEND, contextMenuUri, mActivity, BPSend.class));
+				startActivity(new Intent(Intent.ACTION_SEND, contextMenuUri, getActivity(), BPSend.class));
 				return true;
 			}
 			return false;
@@ -294,14 +286,14 @@ public class BPListFragment extends ListFragment implements OnClickListener {
 	
 	private void deleteRecord() {
 		Uri contextMenuUri = ContentUris.withAppendedId(mIntent.getData(), mContextMenuRecordId);
-		mActivity.getContentResolver().delete(contextMenuUri, null, null);
+		getActivity().getContentResolver().delete(contextMenuUri, null, null);
 	}
 	
 	private class DeleteDialogFragment extends DialogFragment {
 
 		@Override
 		public Dialog onCreateDialog(Bundle savedInstanceData) {
-			AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 			builder.setMessage(getString(R.string.really_delete))
 				.setCancelable(false)
 				.setPositiveButton(getString(R.string.label_yes), new DialogInterface.OnClickListener() {
@@ -331,7 +323,7 @@ public class BPListFragment extends ListFragment implements OnClickListener {
 					|| Intent.ACTION_GET_CONTENT.equals(action)) {
 				// The caller is waiting for us to return a note selected by
 				// the user. The have clicked on one, so return it now.
-				mActivity.setResult(Activity.RESULT_OK, new Intent().setData(uri));
+				getActivity().setResult(Activity.RESULT_OK, new Intent().setData(uri));
 			} else {
 				// Launch activity to view/edit the currently selected item
 				startActivity(new Intent(Intent.ACTION_EDIT, uri));
