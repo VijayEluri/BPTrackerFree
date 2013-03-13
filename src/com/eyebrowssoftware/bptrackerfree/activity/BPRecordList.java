edd12d33b8c4b4b753797bc59997b61dff333447
Brion Emde <brione2001@gmail.com>
2013-03-12 21:45:15 -0700

    Changed the id-based signatures in the listener to uri-based
    For whatever reason, the very same code that didn't seem to work
    at work, works just fine on the emulator at home. I'm tempted to
    try it out on the tablet. It's getting late.

--- src/com/eyebrowssoftware/bptrackerfree/activity/BPRecordList.java
+++ src/com/eyebrowssoftware/bptrackerfree/activity/BPRecordList.java
@@ -15,7 +15,6 @@
  */
 package com.eyebrowssoftware.bptrackerfree.activity;
 
-import android.content.ContentUris;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
